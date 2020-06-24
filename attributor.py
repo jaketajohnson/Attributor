877c929e79c3033799a63b7889b39b52ac785793
Jake Johnson <56829096+jaketajohnson@users.noreply.github.com>
2020-06-24 09:20:52 -0500

    Restructuring to improve performance
    Initial check for empty selections
    Added filter for gravity mains
    Learned how to use docstrings
    Modified code blocks and comments

--- attributor.py
+++ attributor.py
@@ -26,6 +26,7 @@ from logging.handlers import RotatingFileHandler
 
 
 def start_rotating_logging(log_file=None, max_bytes=100000, backup_count=1, suppress_requests_messages=True):
+    """Creates a logger that outputs to stdout and a log file; outputs start and completion of functions or attribution of functions"""
 
     formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
@@ -63,13 +64,12 @@ def start_rotating_logging(log_file=None, max_bytes=100000, backup_count=1, supp
 
 
 def Attributor():
+    """Collection of attribution functions"""
 
     # Paths
     fgdb_folder = r"F:\Shares\FGDB_Services"
     sde = os.path.join(fgdb_folder, r"DatabaseConnections\COSPW@imSPFLD@MCWINTCWDB.sde")
     temp_fgdb = os.path.join(fgdb_folder, r"Data\Attributor.gdb")
-
-    # Environment
     arcpy.env.overwriteOutput = True
     arcpy.env.workspace = r"memory\tempData"
 
@@ -80,73 +80,75 @@ def Attributor():
     spatial_id_line_storm = "!SPATIALSTART! + '_' + !SPATIALEND!"
     spatial_id_point = "str(int(!NAD83X!))[2:4] + str(int(!NAD83Y!))[2:4] + '-' + str(int(!NAD83X!))[4] + str(int(!NAD83Y!))[4] + '-' + str(int(!NAD83X!))[-2:] + str(int(!NAD83Y!))[-2:]"
 
-    # Special case expressions
-    sewer_stormwater_storm = "WATERTYPE = 'SW' And FACILITYID IS NULL"
-    sewer_stormwater_private = "OWNEDBY = -2 And FACILITYID IS NULL"
+    # Expression to select manholes or inlets to calculate a Facility ID for (if it's stormwater or non-city owned)
+    manhole_exception = "(WATERTYPE = 'SW' or OWNEDBY = -2) And FACILITYID IS NULL"
+    cleanout_exception = "OWNEDBY = -2 and FACILITYID IS NULL"
 
-    def facility_id_exceptions(asset, filter_selection):
-        # Template for calculating FACILITYID under special cases (e.g.: stormwater sewers in sewer tables or private sewers)
-        # [0] = input asset, [1] = expression to filter features
-        selection = arcpy.SelectLayerByAttribute_management(asset, "NEW_SELECTION", filter_selection)
-        if int(arcpy.GetCount_management(selection).getOutput(0)) > 0:
-            arcpy.CalculateField_management(selection, "FACILITYID", "!SPATIALID!", "PYTHON3")
-        else:
-            pass
+    # Storing the date one week from time of running as a filter
+    today = datetime.date.today()
+    week_ago = today - datetime.timedelta(days=1)
+    week_ago = week_ago.strftime(r"%m/%d/%Y %H:%M:%S %p")
 
     def sewer_attribution():
+        """Attribute sewer assets
 
-        # Feature Classes
+        Takes the list of assets and their type then uses it to calculate the geometry and spatial fields. If the asset is a noted exception, calculate the Facility ID.
+            * Gravity mains: privately owned sewers or stormwater sewers that output to a combined sewer.
+            * Cleanouts: privately owned sewers
+            * Inlets: all inlets
+
+        """
+
+        # Paths
         sewer = os.path.join(sde, "SewerStormwater")
         sewer_main = os.path.join(sewer, "ssGravityMain")
         sewer_manhole = os.path.join(sewer, "ssManhole")
         sewer_cleanout = os.path.join(sewer, "ssCleanout")
         sewer_inlet = os.path.join(sewer, "ssInlet")
-
-        sewer_assets = [[sewer_main, "line", "Sewer Mains"],
-                        [sewer_manhole, "point", "Sewer Manholes"],
+        sewer_assets = [[sewer_manhole, "point", "Sewer Manholes"],
                         [sewer_cleanout, "point", "Sewer Cleanouts"],
-                        [sewer_inlet, "point", "Sewer Inlets"]]
+                        [sewer_inlet, "point", "Sewer Inlets"],
+                        [sewer_main, "line", "Sewer Mains"]]
 
+        # Attribution
         for asset in sewer_assets:
-
-            # Make a temp feature layer then calculate fields depending on if asset is a point or a line
+            logger.info(f"--- --- {asset[2]} Start")
             arcpy.MakeFeatureLayer_management(asset[0], "asset_temp")
+            selection_by_date = arcpy.SelectLayerByAttribute_management("asset_temp", "NEW_SELECTION", f"LASTUPDATE >= '{week_ago}'")
 
-            if asset[1] == "line":
-                logger.info(f"--- --- {asset[2]} Start")
-                arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83XSTART", "LINE_START_X"],
-                                                                            ["NAD83YSTART", "LINE_START_Y"],
-                                                                            ["NAD83XEND", "LINE_END_X"],
-                                                                            ["NAD83YEND", "LINE_END_Y"]])
-                arcpy.CalculateFields_management("asset_temp", "PYTHON3", [["SPATAILSTART", spatial_start],
-                                                                           ["SPATAILEND", spatial_end],
-                                                                           ["SPATIALID", spatial_id_line_sewer]])
-                facility_id_exceptions(asset[0], sewer_stormwater_storm)
-                facility_id_exceptions(asset[0], sewer_stormwater_private)
-                logger.info(f"--- --- {asset[2]} Complete")
-
-            elif asset[1] == "point":
-                logger.info(f"--- --- {asset[2]} Start")
-                arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83X", "POINT_X"],
-                                                                            ["NAD83Y", "POINT_Y"]])
-                arcpy.CalculateField_management("asset_temp", "SPATIALID", spatial_id_point, "PYTHON3")
-
-                # Only sewer_inlet and sewer_manhole need to have Facility ID calculated for type points
+            # Attribution
+            if int(arcpy.GetCount_management(selection_by_date).getOutput(0)) > 0:
+                if asset[1] == "point":
+                    arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83X", "POINT_X"],
+                                                                                ["NAD83Y", "POINT_Y"]])
+                    arcpy.CalculateField_management("asset_temp", "SPATIALID", spatial_id_point, "PYTHON3")
+                elif asset[1] == "line":
+                    arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83XSTART", "LINE_START_X"],
+                                                                                ["NAD83YSTART", "LINE_START_Y"],
+                                                                                ["NAD83XEND", "LINE_END_X"],
+                                                                                ["NAD83YEND", "LINE_END_Y"]])
+                    arcpy.CalculateFields_management("asset_temp", "PYTHON3", [["SPATAILSTART", spatial_start],
+                                                                               ["SPATAILEND", spatial_end],
+                                                                               ["SPATIALID", spatial_id_line_sewer]])
                 if asset[0] == sewer_manhole:
-                    facility_id_exceptions(asset[0], sewer_stormwater_storm)
-                    facility_id_exceptions(asset[0], sewer_stormwater_private)
+                    selection = arcpy.SelectLayerByAttribute_management("asset_temp", "NEW_SELECTION", manhole_exception)
+                    arcpy.CalculateField_management(selection, "FACILITYID", "!SPATIALID!", "PYTHON3")
+                elif asset[0] == sewer_cleanout:
+                    selection = arcpy.SelectLayerByAttribute_management(asset[0], "NEW_SELECTION", cleanout_exception)
+                    arcpy.CalculateField_management(selection, "FACILITYID", "!SPATIALID!", "PYTHON3")
                 elif asset[0] == sewer_inlet:
                     arcpy.CalculateField_management(asset[0], "FACILITYID", "!SPATIALID!", "PYTHON3")
-                elif asset[0] == sewer_cleanout:
-                    facility_id_exceptions(asset[0], sewer_stormwater_private)
-                logger.info(f"--- --- {asset[2]} Complete")
 
-            else:
-                pass
+            logger.info(f"--- --- {asset[2]} Complete")
 
     def storm_attribution():
+        """Attribute storm assets
 
-        # Feature Classes
+        Takes the list of assets and their type then uses it to calculate the geometry and spatial fields.
+
+        """
+
+        # Paths
         storm = os.path.join(sde, "Stormwater")
         storm_main = os.path.join(storm, "swGravityMain")
         storm_manhole = os.path.join(storm, "swManhole")
@@ -154,7 +156,6 @@ def Attributor():
         storm_inlet = os.path.join(storm, "swInlet")
         storm_discharge = os.path.join(storm, "swDischargePoint")
         storm_culvert = os.path.join(storm, "swCulvert")
-
         storm_assets = [[storm_main, "line", "Storm Mains"],
                         [storm_manhole, "point", "Storm Manholes"],
                         [storm_cleanout, "point", "Storm Cleanouts"],
@@ -162,13 +163,14 @@ def Attributor():
                         [storm_discharge, "point", "Storm Discharge Points"],
                         [storm_culvert, "line", "Storm Culverts"]]
 
+        # Attribution
         for asset in storm_assets:
 
             # Make a temp feature layer then calculate fields depending on if asset is a point or a line
+            logger.info(f"--- --- {asset[2]} Start")
             arcpy.MakeFeatureLayer_management(asset[0], "asset_temp")
 
             if asset[1] == "line":
-                logger.info(f"--- --- {asset[2]} Start")
                 arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83XSTART", "LINE_START_X"],
                                                                             ["NAD83YSTART", "LINE_START_Y"],
                                                                             ["NAD83XEND", "LINE_END_X"],
@@ -177,39 +179,38 @@ def Attributor():
                                                                            ["SPATIALEND", spatial_end],
                                                                            ["SPATIALID", spatial_id_line_storm],
                                                                            ["FACILITYID", spatial_id_line_storm]])
-                logger.info(f"--- --- {asset[2]} Complete")
-
             elif asset[1] == "point":
-                logger.info(f"--- --- {asset[2]} Start")
                 arcpy.CalculateGeometryAttributes_management("asset_temp", [["NAD83X", "POINT_X"],
                                                                             ["NAD83Y", "POINT_Y"]])
                 arcpy.CalculateFields_management("asset_temp", "PYTHON3", [["SPATIALID", spatial_id_point],
                                                                            ["FACILITYID", spatial_id_point]])
-                logger.info(f"--- --- {asset[2]} Complete")
-            else:
-                pass
+            logger.info(f"--- --- {asset[2]} Complete")
 
     def gps_attribution():
+        """Append new GPS shapefiles in the Y: drive to the gpsNode feature class on the SDE then calculate their facility ID
 
-        # Feature Classes
-        engineering = os.path.join(sde, "SewerEngineering")
-        gps_points = os.path.join(engineering, "gpsNode")
+        Use regex and the text file to sort through folders in the Y: drive to find those with the correct formatting and create a list of those. Use this list to append the shapefiles
+        within the folders to gpsNode. After this, update the text file with the current date. Any folder with a date before this new date will not be appended next runtime.
+
+        """
 
         # Paths
+        engineering = os.path.join(sde, "SewerEngineering")
+        gps_points = os.path.join(engineering, "gpsNode")
         shape_folder = "Y:\\"
 
-        # Grab last updated date from a text file, define a date format to use (YYYY-mm-dd)
+        # Grab last updated date from a text file
         f = open("last_updated.txt", "r")
         last_updated = [f.read()]
         datetime_format = "%Y-%m-%d"
 
-        # Using os.walk, append folders to folder_list if the folder name has a dash and no letters, period, or spaces.
+        # Create a list of folders to be appended
         folder_list = []
         for root, dirs, files in os.walk(shape_folder, topdown=False):
             [folder_list.append(name) for name in dirs if re.search("[-]", name) and not re.search("[a-zA-z. ]", name) and
              datetime.datetime.strptime(last_updated[0], datetime_format) < datetime.datetime.strptime(name, datetime_format)]  # Add any folder without a letter in it
 
-        # If the list is not empty, select files in each folder that end in " insMACP.shp" and append them to SewerEngineering/gpsNodes
+        # Append new folders
         if len(folder_list) > 0:
             for folder in folder_list:
                 directory = os.path.join(shape_folder, folder)
@@ -241,52 +242,44 @@ def Attributor():
                                                 fr'GEOID "GEOID" true true false 20 Text 0 0,First,#', '', '')
             arcpy.CalculateField_management(gps_points, "SPATIALID", spatial_id_point, "PYTHON3")
 
-            # Update last_update.txt to show new date. Next time this runs it will compare folder names to the date this last ran. To work you MUST close the file after writing
+            # Update last_update.txt
             new_date = datetime.datetime.now().strftime(datetime_format)
             f = open("last_updated.txt", "w")
             f.write(f"{new_date}")
             f.close()
-        else:
-            pass
 
     def ward_attribution():
-
-        # Feature Classes
-        sewer = os.path.join(sde, "SewerStormwater")
-        sewer_main = os.path.join(sewer, "ssGravityMain")
+        """Attribute the sewer main GXPCity field using the administrative area polygon's label field its center is in. If it's a ward, add text to the label."""
 
         # Paths
-        area = os.path.join(temp_fgdb, "AdministrativeArea")  # Townships and wards polygons
+        sewer = os.path.join(sde, "SewerStormwater")
+        sewer_main = os.path.join(sewer, "ssGravityMain")
+        area = os.path.join(temp_fgdb, "AdministrativeArea")  # Townships and wards polygon
 
+        # Attribution
         with arcpy.da.SearchCursor(area, ["OID@", "Label"]) as cursor:
             for row in cursor:
-
-                # Uses the OID to create a label to be used as the city field in sewer_mains
                 if re.search(fr"\bWard\b", str(row[1])):
                     city = f"'Springfield ({row[1]})'"
                 else:
                     city = f"'{row[1]}'"
-
-                # Selects sewer mains inside the current area polygon
                 selection = arcpy.SelectLayerByAttribute_management(area, "NEW_SELECTION", fr"OBJECTID = {row[0]}")
                 sewer_selection = arcpy.SelectLayerByLocation_management(sewer_main, "HAVE_THEIR_CENTER_IN", selection, None, "NEW_SELECTION")
-
                 arcpy.CalculateField_management(sewer_selection, 'GXPCity', f"{city}", 'PYTHON3')
         del cursor
 
     def pond_attribution():
+        """Calculate the Facility ID of detention ponds using its centroid coordinates"""
 
-        # Feature Classes
-        storm = os.path.join(sde, "Stormwater Network")
+        # Paths
+        storm = os.path.join(sde, "Stormwater")
         detention_areas = os.path.join(storm, "swDetention")
 
-        # Create a facility ID string from the geometry properties
         facility_id = "str(!SHAPE!.centroid.X)[2:4] + str(!SHAPE!.centroid.Y)[2:4] + '-' + str(!SHAPE!.centroid.X)[4] + str(!SHAPE!.centroid.Y)[4] + '-' + " \
                       "str(!SHAPE!.centroid.X)[-2:] + str(!SHAPE!.centroid.Y)[-2:]"
-
         arcpy.CalculateField_management(detention_areas, "FACILITYID", facility_id, "PYTHON3")
 
-    # Paths to desired log file
+    # Log file paths
     scripts_root = r"C:\Scripts"
     script_name_no_ext = os.path.splitext(os.path.basename(sys.argv[0]))[0]
     script_folder = os.path.join(scripts_root, script_name_no_ext)
@@ -342,7 +335,6 @@ def Attributor():
             print(arcpy.GetMessages(2))
 
     finally:
-        # Shut down logging
         try:
             logger.info("--- Script Execution Completed ---")
             logging.shutdown()
